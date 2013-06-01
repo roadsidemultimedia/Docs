17a037418d1dbe5a377888a076ebcc7cc1049894
Andrew Powers <andrew@pagelines.com>
2013-05-31 17:40:37 -0700

    nav detection

---
+++ gh-pages-template/public/js/common.js
@@ -0,0 +1,44 @@
+!function ($) {
+
+	// Load After Page Load
+	$(document).ready(function() {
+	
+		$.plNavigation.init()
+		
+	})
+	
+	// Load After Images
+	$(window).load(function() {
+	
+	})
+	
+	$.plNavigation = {
+		
+		init: function(){
+			
+			$(this).detectCurrentPageAndNav()
+			
+		}
+		
+		, detectCurrentPageAndNav: function(){
+			
+			var url = window.location;
+			
+			// Will also work for relative and absolute hrefs
+			$('.nav-detect a').filter(function() {
+				
+			    return this.href == url
+			
+			}).addClass('page-active')
+			
+			$('.nav-detect .lead a').filter(function() {
+				
+			    return this.href == url
+			
+			}).closest('.nav-list').addClass('menu-active')
+			
+		}
+		
+	}
+	
+}(window.jQuery);
